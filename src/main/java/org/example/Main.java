3d845cd131ce78cd1e0522f3c4003227c585f778
KozlovMV <KozlovMV@kadastr.ru>
2023-10-24 23:51:49 +0300

    Task1 completed!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int taskNumber;
+        try (Scanner userInput = new Scanner(System.in)) {
+            taskNumber = userInput.nextInt();
+        }
+        switch (taskNumber) {
+            case 1      -> System.out.println(day1_count());
+            default     -> System.out.println("task not found");
+        }
+    }
+
+    public static int day1_count() {
+        Path path = Paths.get("./src/main/resources/Day1/input1.txt");
+        File inputFile = new File(path.toUri());
+        int previous;
+        int counter = 0;
+        try (Scanner scanner = new Scanner(inputFile, StandardCharsets.UTF_8)) {
+            previous = scanner.nextInt();
+            while (scanner.hasNext()) {
+                int measurement = scanner.nextInt();
+                if (measurement > previous) {
+                    counter += 1;
+                }
+                previous = measurement;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return counter;
     }
 }
